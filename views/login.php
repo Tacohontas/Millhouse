9a87d838f5d1f5cb0a38d5727388976d8bb82224
Tacohontas <Jonathan.undhagen@gmail.com>
2020-03-06 14:20:29 +0100

    Update login.php

--- views/login.php
+++ views/login.php
@@ -2,20 +2,6 @@
 include("./includes-partials/header.php")
 ?>
 
-<?php
-
-// TA BORT
-
-if (isset($_GET['error']) && !empty($_GET['error'])) {
-    if($_GET['error'] == 'emptyvalues'){
-        echo "Du måste fylla i alla fält";
-    } else {
-        echo "Det gick inte att logga in";
-    }
-};
-?>
-
-
 <form action="./handlers/handle_login.php" method="post">
     <input type="text" placeholder="Username" name="username" required>
     <input type="password" placeholder="Password" name="password" required>
