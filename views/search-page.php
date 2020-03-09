27e27dc7fa2a5fefe8e150f55c1ea1e2472136c9
Tacohontas <Jonathan.undhagen@gmail.com>
2020-03-09 11:50:12 +0100

    Update search-page.php

--- views/search-page.php
+++ views/search-page.php
@@ -5,43 +5,41 @@ include("./includes-partials/database_connection.php");
 ?>
 
 <div class="search-form">
-<form method="GET" action="./handlers/handle_search.php">
-    <input class="search-input" type="text" name="searchQ" placeholder="Search" required>
-    <input class="search" type="submit" value="Sök">
-</form>
+    <form method="GET" action="./handlers/handle_search.php">
+        <input class="search-input" type="text" name="searchQ" placeholder="Search" required>
+        <input class="search" type="submit" value="Sök">
+    </form>
 </div>
 <?php
- if (!empty($_GET['searchQ']) ) {
+if (!empty($_GET['searchQ'])) {
 
     echo "<h2 class='search-title'>Sökresultat</h2>";
 
     $BlogPosts = new BLOGPOST($dbh);
-    $BlogPosts->searchBlogPosts($_GET['searchQ']);
+    $BlogPosts->searchBlogPosts(htmlentities($_GET['searchQ']));
 
-    foreach( $BlogPosts->getBlogPosts() as $BlogPost) {
-    echo "<div class='blogpost'>";
-    echo "<h2 class='heading'>". $BlogPost['title'] . "</h2>";
-    echo $BlogPost['date_posted'];
-    if(isset($BlogPost['img'])){
-    echo "<img class='blogpost_img' src='images/{$BlogPost['img']}' alt='Här ska det va en bild' maxheight=800 width=380>";
+
+    if (!empty($BlogPosts->getBlogPosts())) {
+        foreach ($BlogPosts->getBlogPosts() as $BlogPost) {
+            echo "<div class='blogpost'>";
+            echo "<h2 class='heading'>" . $BlogPost['title'] . "</h2>";
+            echo $BlogPost['date_posted'];
+            if (isset($BlogPost['img'])) {
+                echo "<img class='blogpost_img' src='images/{$BlogPost['img']}' alt='Här ska det va en bild' maxheight=800 width=380>";
+            };
+            echo "<div class='blogpost_content'>" . $BlogPost['content'] . "</div>";
+            echo "</div>";
         };
-    echo "<div class='blogpost_content'>" . $BlogPost['content'] ."</div>";
-    echo "</div>";
-
-        // --- Hämta länkade inlägg ---//
-    /* echo "<div class='blogpost_section'>";
-    echo "<h2><a href='index.php?page=view&postId=" . $BlogPost['id'] .">". $BlogPost['title'] ."</a> </h2>";
-    if(isset($BlogPost['img'])){
-    echo "<img class='blogpost_img' src='images/". $BlogPost['img'] . "alt='Här ska det va en bild' maxheight=600 width=300>";
-    };
-    $BlogPost['date_posted'];
-    echo "</div>"; */
-
-    };
+    } else {
+        // fixa i errorrutan
+        $errorMessages = "Hoppsan! Det finns inga inlägg som innehåller det sökordet.";
+        header("location:./index.php?page=search&error=true&errormessage={$errorMessages}");
+        die;
+    }
 } else {
-        echo "";
+    return false;
 }
- 
+
 ?>
 
 
